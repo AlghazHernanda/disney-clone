d97a8a5462436ed7f237a78c71ce88894b841fbb
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2022-06-14 22:56:25 +0700

    id detail

---
+++ pages/movie/[id].js
@@ -0,0 +1,10 @@
+import React from 'react'
+//page ini berfungsi untuk movie detail karena ngambil dari id
+
+function Movie() {
+  return (
+    <div>movie</div>
+  )
+}
+
+export default Movie
---
+++ pages/show/[id].js
@@ -0,0 +1,10 @@
+import React from 'react'
+//page ini berfungsi untuk movie detail karena ngambil dari id
+
+function Show() {
+  return (
+    <div>show</div>
+  )
+}
+
+export default Show
