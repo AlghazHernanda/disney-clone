1922f362c0c6a2a3f57e0b9983b99da767812fea
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2022-06-15 10:48:26 +0700

    backend

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -1,10 +1,29 @@
-import React from 'react'
 //page ini berfungsi untuk movie detail karena ngambil dari id
+import React from 'react'
+import { getSession, useSession } from "next-auth/client";
 
-function Movie() {
+function Movie({ result }) {
   return (
-    <div>movie</div>
+    <div>
+        
+    </div>
   )
 }
 
 export default Movie
+
+export async function getServerSideProps(context) {
+    const session = await getSession(context);
+    const { id } = context.query;
+  
+    const request = await fetch(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.API_KEY}&language=en-US&append_to_response=videos`
+    ).then((response) => response.json());
+  
+    return {
+      props: {
+        session,
+        result: request,
+      },
+    };
+  }
