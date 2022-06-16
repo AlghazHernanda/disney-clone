784a76df6bada87f60bebd437655cd4678ae08ed
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2022-06-16 11:43:17 +0700

    overlay

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -5,7 +5,7 @@ import Head from "next/head";
 import Header from "../../components/Header";
 import Hero from "../../components/Hero";
 import Image from "next/image";
-import { PlusIcon } from '@heroicons/react/solid';
+import { PlusIcon, XIcon } from '@heroicons/react/solid';
 
 function Movie({ result }) {
 const [session] = useSession();
@@ -83,8 +83,28 @@ const [showPlayer, setShowPlayer] = useState(false);
                     {result.genres.map((genre) => genre.name + " ")}{" "}
                 </p>
                 <h4 className="text-sm md:text-lg max-w-4xl">{result.overview}</h4>
-
             </div>
+
+             {/* Bg Overlay */}
+             {showPlayer && (
+                <div className="absolute inset-0 bg-black opacity-50 h-full w-full z-50"></div>
+              )}
+
+              <div  className={`absolute top-3 inset-x-[7%] md:inset-x-[13%] rounded overflow-hidden transition duration-1000 ${
+              showPlayer ? "opacity-100 z-50" : "opacity-0"
+            }`}>
+
+                <div className="flex items-center justify-between bg-black text-[#f9f9f9] p-3.5">
+                  <span className="font-semibold">Play Trailer</span>
+                  <div
+                    className="cursor-pointer w-8 h-8 flex justify-center items-center rounded-lg opacity-50 hover:opacity-75 
+                    hover:bg-[#0F0F0F]"
+                    onClick={() => setShowPlayer(false)}
+                  >
+                    <XIcon className="h-5" />
+                  </div>
+                </div>
+              </div>
           
         </section>
       )
